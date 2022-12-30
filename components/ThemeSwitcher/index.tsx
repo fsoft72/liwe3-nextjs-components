7a4f02b86f619e3c111587f89b2b724bb53f94e8
fsoft <fabio.rotondo@gmail.com>
2022-12-30 15:46:40 +0100

    New ThemeSwitcher

--- components/ThemeSwitcher/index.tsx
+++ components/ThemeSwitcher/index.tsx
@@ -1,17 +1,26 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../ThemeContext';
 
+import classes from './index.module.scss';
+
 // Create a component that allows the user to switch between the light and dark theme
 export function ThemeSwitcher () {
 	const { theme, setTheme } = useContext( ThemeContext );
 
-	const toggleTheme = () => {
-		theme === 'light' ? setTheme( 'dark' ) : setTheme( 'light' );
+	const toggleTheme = ( e: any ) => {
+		console.log( e.target );
+
+		if ( e.target.checked ) {
+			setTheme( 'dark' );
+		} else {
+			setTheme( 'light' );
+		}
 	};
 
 	return (
-		<button onClick={toggleTheme}>
-			Toggle theme (currently {theme})
-		</button>
+		<label className={classes.switch}>
+			<input type="checkbox" onChange={toggleTheme} />
+			<span className={classes.slider}></span>
+		</label>
 	);
 }
